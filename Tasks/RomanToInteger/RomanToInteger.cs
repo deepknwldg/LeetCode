7d78c30a4d200e050493052430b633a1bf5092f7
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-11-24 00:00:44 +0300

    roman to int issue

---
+++ Tasks/RomanToInteger/RomanToInteger.cs
@@ -0,0 +1,29 @@
+﻿namespace Tasks.RomanToInteger
+{
+    public static class RomanToInteger
+    {
+        public static int RomanToInt(string s)
+        {
+            var alphabet = new Dictionary<char, int> {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+
+            var length = s.Length - 1;
+            var total = alphabet[s[length]];
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                var current = alphabet[s[i]];
+                total += (alphabet[s[i + 1]] > current ? -1 : 1) * current;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ Tests/RomanToIntegerTests/RomanToIntegerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Tasks.RomanToInteger;
+
+namespace Tests.RomanToIntegerTests
+{
+    public class RomanToIntegerTests
+    {
+        [Test]
+        [TestCase("III", ExpectedResult = 3)]
+        [TestCase("IV", ExpectedResult = 4)]
+        [TestCase("IX", ExpectedResult = 9)]
+        [TestCase("LVIII", ExpectedResult = 58)]
+        [TestCase("MCMXCIV", ExpectedResult = 1994)]
+        public int RomanToInt(string roman)
+        {
+            return RomanToInteger.RomanToInt(roman);
+        }
+    }
+}
