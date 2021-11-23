8f973df14d8748ae5d6a2d5ecec65af831155e0e
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-11-23 23:26:27 +0300

    rename folder

---
+++ Tasks/PalindromeNumber/PalindromeNumber.cs
@@ -0,0 +1,23 @@
+﻿namespace Tasks.PalindromeNumber
+{
+    public static class PalindromeNumber
+    {
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            int revertedValue = 0;
+
+            while (x > revertedValue)
+            {
+                revertedValue = revertedValue * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == revertedValue || x == revertedValue / 10;
+        }
+    }
+}
