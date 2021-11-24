86cfd0e7e57c02f6128d891877f6fb7d6b419052
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-11-25 00:27:31 +0300

    reverseBits

---
+++ Tests/EasyTests/ReverseBitsTests/ReverseBitsTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasks.Easy.ReverseBits;
+
+namespace Tests.EasyTests.ReverseBitsTests
+{
+    internal class ReverseBitsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(4294967293, 3221225471)]
+        [TestCase(43261596, 964176192)]
+        public void ReverseBitsMethod(uint x, uint expected)
+        {
+            uint @in = (uint)x;
+            var actual = ReverseBits.ReverseBitsMethod(@in);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
