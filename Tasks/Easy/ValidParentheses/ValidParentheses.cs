826a1ae3977e186fd98a8b32398bed1893e3d12e
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-11-28 17:42:29 +0300

    add solution valid parentheses

--- Tasks/Easy/ValidParentheses/ValidParentheses.cs
+++ Tasks/Easy/ValidParentheses/ValidParentheses.cs
@@ -2,8 +2,22 @@ public static class ValidParentheses
 {
     public static bool IsValid(string s)
     {
-        
+        var parenthesesStack = new Stack<char>();
 
-        return true;
+        foreach (var ch in s)
+        {
+            if (ch == '(')
+                parenthesesStack.Push(')');
+            else if (ch == '[')
+                parenthesesStack.Push(']');
+            else if (ch == '{')
+                parenthesesStack.Push('}');
+            else if (parenthesesStack.Count == 0 || parenthesesStack.Pop() != ch)
+            {
+                return false;
+            }
+        }
+
+        return parenthesesStack.Count == 0;
     }
 }
