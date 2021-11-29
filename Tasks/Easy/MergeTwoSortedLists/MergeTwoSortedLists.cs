9ba7f501be6939104dca8d50b4fdf34672b1f95c
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-11-29 20:58:37 +0300

    implimentation

---
+++ Tasks/Easy/MergeTwoSortedLists/MergeTwoSortedLists.cs
@@ -0,0 +1,24 @@
+﻿namespace Tasks.Easy.MergeTwoSortedLists
+{
+    public static class MergeTwoSortedLists
+    {
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null)
+                return list2;
+            if (list2 == null)
+                return list1;
+
+
+            if (list2.val > list1.val)
+            {
+                list1.next = MergeTwoLists(list1.next, list2);
+
+                return list1;
+            }
+
+            list2.next = MergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
