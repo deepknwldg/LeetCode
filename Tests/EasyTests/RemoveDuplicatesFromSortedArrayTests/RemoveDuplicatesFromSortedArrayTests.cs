30f0c50b28f217b1b3433efdaffd9145e9f3dc8f
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-11-30 20:28:12 +0300

    add test

---
+++ Tests/EasyTests/RemoveDuplicatesFromSortedArrayTests/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Tasks.Easy.RemoveDuplicatesFromSortedArray;
+
+namespace Tests.EasyTests.RemoveDuplicatesFromSortedArrayTests
+{
+    [TestFixture]
+    public class RemoveDuplicatesFromSortedArrayTests
+    {
+
+        [Test]
+        public void Method1()
+        {
+            Assert.AreEqual(5, RemoveDuplicatesFromSortedArray.RemoveDuplicates(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }));
+        }
+    }
+}
