1260967dda04ebb0e32b911eab28d066a97208a4
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-11-30 20:28:05 +0300

    add solve task

---
+++ Tasks/Easy/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,24 @@
+﻿namespace Tasks.Easy.RemoveDuplicatesFromSortedArray
+{
+    public static class RemoveDuplicatesFromSortedArray
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            int i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    nums[i++ + 1] = nums[j];
+                }
+            }
+
+            ++i;
+
+            return i;
+        }
+    }
+}
