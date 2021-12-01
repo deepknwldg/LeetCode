ab38d0e4df4c1a5fdde8f90b311fdf316af3372e
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-12-01 20:13:55 +0300

    solution and description task

---
+++ Tasks/Easy/RemoveElement/RemoveElement.cs
@@ -0,0 +1,20 @@
+﻿namespace Tasks.Easy.RemoveElement
+{
+    public static class RemoveElement
+    {
+        public static int RemoveElementMethod(int[] nums, int val)
+        {
+            int j = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[j] = nums[i];
+                    j++;
+                }
+            }
+
+            return j;
+        }
+    }
+}
