28eb215fd356c232e06aa8be716e6d4b9f21f622
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-12-02 20:22:12 +0300

    solution

---
+++ Tasks/Hard/ReducingDishes/ReducingDishes.cs
@@ -0,0 +1,22 @@
+﻿namespace Tasks.Hard.ReducingDishes
+{
+    public static class ReducingDishes
+    {
+        public static int MaxSatisfaction(int[] satisfaction)
+        {
+            Array.Sort(satisfaction);
+            if (satisfaction[^1] < 0)
+                return 0;
+
+            int result = 0;
+            int total = 0;
+            for (int i = satisfaction.Length - 1; i >= 0 && satisfaction[i] > -total; i--)
+            {
+                total += satisfaction[i];
+                result += total;
+            }
+
+            return result;
+        }
+    }
+}
