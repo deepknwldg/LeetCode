ae5365970d8a5f7de698de2a383d26b79cb50261
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-12-02 20:22:18 +0300

    test

---
+++ Tests/HardTests/ReducingDishesTests/ReducingDishesTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Tasks.Hard.ReducingDishes;
+
+namespace Tests.HardTests.ReducingDishesTests
+{
+    [TestFixture]
+    internal class ReducingDishesTests
+    {
+        [Test]
+        public void Method1()
+        {
+            Assert.AreEqual(14, ReducingDishes.MaxSatisfaction(new int[] { -1, -8, 0, 5, -9 }));
+        }
+
+        [Test]
+        public void Method2()
+        {
+            Assert.AreEqual(20, ReducingDishes.MaxSatisfaction(new int[] { 4, 3, 2 }));
+        }
+
+        [Test]
+        public void Method3()
+        {
+            Assert.AreEqual(0, ReducingDishes.MaxSatisfaction(new int[] { -1, -4, -5 }));
+        }
+
+        [Test]
+        public void Method4()
+        {
+            Assert.AreEqual(35, ReducingDishes.MaxSatisfaction(new int[] { -2, 5, -1, 0, 3, -3 }));
+        }
+    }
+}
